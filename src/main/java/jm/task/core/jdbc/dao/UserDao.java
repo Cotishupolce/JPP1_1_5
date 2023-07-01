105ad0a846727d6a6c4e6ac438506cf4c6e094e2
Cotishupolce <wolc.man00@yandex.ru>
2023-07-01 17:37:34 +0300

    add UserDao and UserDaoJDBCImpl

--- src/main/java/jm/task/core/jdbc/dao/UserDao.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDao.java
@@ -2,19 +2,21 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface UserDao {
-    void createUsersTable();
 
-    void dropUsersTable();
+    void createUsersTable() throws SQLException;
 
-    void saveUser(String name, String lastName, byte age);
+    void dropUsersTable() throws SQLException;
 
-    void removeUserById(long id);
+    void saveUser(String name, String lastName, byte age) throws SQLException;
 
-    List<User> getAllUsers();
+    void removeUserById(long id) throws SQLException;
+
+    List<User> getAllUsers() throws SQLException;
 
 
-    void cleanUsersTable();
+    void cleanUsersTable() throws SQLException;
 }
